9b2b81b21f7b556dc69e777339bf6be8ee6bb13e
Kyeongrok Kim <oceanfog1@gmail.com>
2025-03-07 16:58:34 +0900

    Update init.lua

--- init.lua
+++ init.lua
@@ -68,6 +68,10 @@ vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {noremap = true})
 vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {noremap = true})
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {noremap = true})
 
+vim.api.nvim_set_keymap('n', '<leader><leader>h', '<C-w>h', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader><leader>l', '<C-w>l', {noremap = true})
+
+
 
 -- pwd
 vim.api.nvim_set_keymap('n', '<leader>p', ':pwd<CR>', {noremap = true})
