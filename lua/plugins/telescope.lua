60893900bc2f4872009f4578db31ab2f9a77a6ed
Kyeongrok <oceanfog1@gmail.com>
2025-05-29 14:46:32 +0900

    [feat] telescope ui 실시간 반영

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -18,8 +18,28 @@ return{
                 print('Checked out to ' .. local_branch)
             end
 
+           -- 실시간 컬러셈 적용용 attach_mappings
+           local function colorscheme_attach_mappings(prompt_bufnr, map)
+             actions.move_selection_next:enhance {
+               post = function()
+                 local entry = action_state.get_selected_entry()
+                 vim.cmd("colorscheme " .. entry.value)
+               end,
+             }
+
+             actions.move_selection_previous:enhance {
+               post = function()
+                 local entry = action_state.get_selected_entry()
+                 vim.cmd("colorscheme " .. entry.value)
+               end,
+             }
+
+             return true
+           end
+
             require('telescope').setup {
                 defaults = {
+                    path_display = { "tail", "smart" },  -- 파일명만 표시
                     mappings = {
                         i = {
                             ["<C-j>"] = actions.move_selection_next,
@@ -37,14 +57,19 @@ return{
                         },
                     },
                 },
+                pickers = {
+                  colorscheme = { enable_preview = true, -- 이미 적용되는 설정일 수도 있음
+                    attach_mappings = colorscheme_attach_mappings
+                  },
+                },
                 layout_config = {
                     horizontal = {
-                        preview_width = 1.0,  -- make the preview window take up the entire width
+                        preview_width = 0.8,  -- make the preview window take up the entire width
                         results_width = 0.01,  -- make the results window very narrow
                     },
                     vertical = {
                         preview_height = 1.0,  -- make the preview window take up the entire height
-                        results_height = 0.01,  -- make the results window very narrow
+                        results_height = 0.05,  -- make the results window very narrow
                     },
                 },
             }
